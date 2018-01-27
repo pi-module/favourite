b98b0e3f58bc7a2e9158000079e353b4429249d1
voltan <djvoltan@gmail.com>
2018-01-27 19:44:52 +0330

    Fix update if event module not install

--- src/Installer/Action/Update.php
+++ src/Installer/Action/Update.php
@@ -61,20 +61,22 @@ class Update extends BasicUpdate
         }
         
         if (version_compare($version, '1.2.6', '<')) {
-           // Alter table field `identity`
-            $tableFav = Pi::db()->prefix('list', 'favourite');
-            $tableExt = Pi::db()->prefix('extra', 'event');
-            $sql = sprintf("UPDATE %s fav INNER JOIN %s ext on ext.id = fav.item SET module = 'event' WHERE module = 'news'", $tableFav, $tableExt);
-            try {
-                $favouriteAdapter->query($sql, 'execute');
-            } catch (\Exception $exception) {
-                $this->setResult('db', array(
-                    'status' => false,
-                    'message' => 'Table alter query failed: '
-                        . $exception->getMessage(),
-                ));
-                return false;
-            }            
+            // Alter table field `identity`
+            if (Pi::service('module')->isActive('event')) {
+                $tableFav = Pi::db()->prefix('list', 'favourite');
+                $tableExt = Pi::db()->prefix('extra', 'event');
+                $sql      = sprintf("UPDATE %s fav INNER JOIN %s ext on ext.id = fav.item SET module = 'event' WHERE module = 'news'", $tableFav, $tableExt);
+                try {
+                    $favouriteAdapter->query($sql, 'execute');
+                } catch (\Exception $exception) {
+                    $this->setResult('db', [
+                        'status'  => false,
+                        'message' => 'Table alter query failed: '
+                            . $exception->getMessage(),
+                    ]);
+                    return false;
+                }
+            }
         }
         return true;
     }
